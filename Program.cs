6e7c2561a6446716a85355ef0ad521b1ce47cc8b
VinceGusmini <vincent.gusmini.sio@gmail.com>
2019-03-20 20:57:21 +0100

    test 1/2

--- Program.cs
+++ Program.cs
@@ -20,5 +20,6 @@ namespace apiEDT
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+                
     }
 }
