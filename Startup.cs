1df877694b3546ed7d3657ecf7118a207140caf4
VinceGusmini <vincent.gusmini.sio@gmail.com>
2019-03-27 11:27:13 +0100

    modif CORS 3

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace apiEDT
                 options.AddPolicy(MyAllowSpecificOrigins,
                 builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200","https://localhost:4200")
+                    builder.WithOrigins()
                                         .AllowAnyHeader()
                                         .AllowAnyMethod()
                                         .SetIsOriginAllowedToAllowWildcardSubdomains();;
